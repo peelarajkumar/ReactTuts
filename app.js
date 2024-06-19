b16c886e0309bcf998abe8fb8cfc7e59112b5677
Raj Kumar Peela <rajkumar.peela@donyati.com>
2024-06-19 10:18:29 +0530

    Second commit - Parcel did a prod build

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
 const heading = React.createElement('h1',{id:"heading",className:'heads'},"hello raj from react ! How are you")
 
 //parent nesetd and siblings
 
-const parent = React.createElement('div', { id: "parentdiv" }, 
-React.createElement('div', { id: "childdiv" }, 
+const parent = React.createElement('div', { id: "parentdiv" },[
+React.createElement('div', { id: "childdiv1" }, 
 [ React.createElement('h1', { id: 'heading1' }, "I am Elder Brother - Rajkumar"),
-  React.createElement('h2', { id: 'heading2' }, "I am younger Brother - RajPaul")
+  React.createElement('h2', { id: 'heading2' }, "I am younger Brother - Rajpaul")
 ]
-))
+),
+React.createElement('div', { id: "childdiv2" }, 
+    [ React.createElement('h1', { id: 'heading3' }, "I am Elder Brother - Shaun Marsh"),
+      React.createElement('h2', { id: 'heading4' }, "I am younger Brother - Mitchell Marsh")
+    ]
+    )
+])
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
